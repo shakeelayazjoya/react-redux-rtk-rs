877cc4977cdb1d811b4dda108248d7001433384f
shakeelayazjoya <shakeelayaz954@gmail.com>
2024-10-23 17:15:30 +0500

    Your commit message here

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-console.log("shakel");
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+const inc = "increment";
+const dec = "decrement";
+const incByAmnt = "incrementByAmount";
+// Create store with middleware
+const store = createStore(reducer, applyMiddleware(logger.default));
+const history = [];
+
+// Corrected reducer logic
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === inc) {
+    return { amount: state.amount + 1 };
+  }
+  if (action.type === dec) {
+    return { amount: state.amount - 1 };
+  }
+
+  if (action.type === incByAmnt) {
+    return { amount: state.amount + action.payload };
+  }
+
+  return state;
+}
+
+// action creators
+
+function increment() {
+  return { type: inc };
+}
+
+function decrement() {
+  return { type: dec };
+}
+
+function incrementByAmount(value) {
+  return { type: incByAmnt, payload: value };
+}
+
+// Global state
+// store.subscribe(() => {
+//   history.push(store.getState()); // Push the new state to history array every time the state changes.
+//   console.log(store.getState());
+// });
+
+// Dispatching actions every 4 seconds
+setInterval(() => {
+  store.dispatch(incrementByAmount(4));
+}, 2000);
+
+// setInterval(() => {
+//   store.dispatch({ type: "incrementByAmount", payload: 4 });
+// }, 4000);
+
+// Initial dispatch to test
+// store.dispatch({ type: "increment" });
